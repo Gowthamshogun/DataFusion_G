08f737ffbae54b8a8b5f389e2fd5b9dede499341
Gowtham Soma Sundara Pandian <gsomasundarapandian@dhanam.cloud>
2026-05-13 11:13:33 +0530

    third

---
+++ frog.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print("Hello " + name + "!")
+print("You are " + age + " years old.")
+print("In 10 years you will be " + str(int(age) + 10) + " years old!")

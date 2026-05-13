ee6c84e9a597dfc932643bdb892da951528c3586
Gowtham Soma Sundara Pandian <gsomasundarapandian@dhanam.cloud>
2026-05-13 11:06:51 +0530

    second

---
+++ pynew_G.py
@@ -0,0 +1,11 @@
+print("--- My first list ---")
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+print("All fruits:", fruits)
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[3])
+
+print("--- Looping through list ---")
+for fruit in fruits:
+    print("I like " + fruit)
